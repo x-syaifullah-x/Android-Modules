3de8fefda5b705649963c6284f3e8165ad7f2050
x-syaifullah-x <x-syaifullah-x@gmail.com>
2024-11-02 22:18:51 +0700

    Update versions.gradle

--- modules/auth/auth-data/src/main/java/id/xxx/module/auth/repository/source/remote/response/Header.kt
+++ modules/auth/auth-data/src/main/java/id/xxx/module/auth/repository/source/remote/response/Header.kt
@@ -1,6 +1,6 @@
 package id.xxx.module.auth.repository.source.remote.response
 
-internal data class Header internal constructor(
+internal class Header internal constructor(
     val code: Int,
     val date: Long,
     val contentLength: Long
--- modules/auth/auth-data/src/main/java/id/xxx/module/auth/repository/source/remote/response/Response.kt
+++ modules/auth/auth-data/src/main/java/id/xxx/module/auth/repository/source/remote/response/Response.kt
@@ -1,6 +1,6 @@
 package id.xxx.module.auth.repository.source.remote.response
 
-internal data class Response<T> internal constructor(
+internal class Response<T> internal constructor(
     val header: Header,
     val body: T
 )
