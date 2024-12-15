f54bbb942e9ff2449c38ad78156a93ced9359790
x-syaifullah-x <x.syaifullah.x@gmail.com>
2024-12-15 10:34:05 +0700

    Fix modules->authentication

---
+++ modules/authentication/presentation/src/main/kotlin/id/xxx/module/ktx/FragmentKtx.kt
@@ -0,0 +1,17 @@
+@file:JvmName("FragmentKtx")
+
+package id.xxx.module.ktx
+
+import androidx.fragment.app.Fragment
+import id.xxx.module.autentication.IAuthentication
+
+internal val Fragment.authentication: IAuthentication?
+    get() = getCallback()
+
+internal inline fun <reified T> Fragment.getCallback() =
+    if (parentFragment is T)
+        parentFragment as? T
+    else if (activity is T)
+        activity as? T
+    else
+        null
